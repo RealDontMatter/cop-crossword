8b645edafcf580e828f87d796d4c94aa65333a12
Maksym Penkov <ipz231_pmo@student.ztu.edu.ua>
2025-10-19 18:34:05 +0300

    Added HintContainer Component

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,5 +1,6 @@
 import { TmpPageSelector } from "./TmpPageSelector.jsx";
 import { GameField } from "./GameField.jsx";
 import { ThemeChanger } from "./ThemeChanger.jsx";
+import { HintContainer } from "./HintContainer.jsx";
 
-export { TmpPageSelector, GameField, ThemeChanger };
+export { TmpPageSelector, GameField, ThemeChanger, HintContainer };
