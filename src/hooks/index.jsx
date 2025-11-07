ee6fe486099dacf0dac4414e409d7a79eea5ada6
Maksym Penkov <ipz231_pmo@student.ztu.edu.ua>
2025-11-07 17:50:12 +0200

    Rewrite settings form validation using react-hook-form

--- src/hooks/index.jsx
+++ src/hooks/index.jsx
@@ -1,4 +1,3 @@
 export {useWarning} from "./useWarning.jsx";
-export {useSettingsFormValidation} from "./useSettingsFormValidation.jsx";
 export {useKeyPress} from "./useKeyPress.jsx"
 export {useTableMove} from "./useTableMove.jsx"
